d6831332ef53bc1cfd2aa3767c134b3e12e240e6
CHOI97 <chlwjrh@gmail.com>
2023-03-22 01:36:05 +0900

    User Flag setting

--- coin_monitoring/app/src/main/java/com/example/coin_monitoring/view/intro/IntroActivity.kt
+++ coin_monitoring/app/src/main/java/com/example/coin_monitoring/view/intro/IntroActivity.kt
@@ -3,11 +3,13 @@ package com.example.coin_monitoring.view.intro
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.Observer
 import com.example.coin_monitoring.MainActivity
 import com.example.coin_monitoring.R
+import com.example.coin_monitoring.databinding.ActivityIntroBinding
 import timber.log.Timber
 
 // Splash 화면 만들기
@@ -16,13 +18,16 @@ import timber.log.Timber
 
 class IntroActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityIntroBinding
     private val viewModel : IntroViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
 
         installSplashScreen()
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
 
         Timber.d("onCreate")
 
@@ -34,6 +39,7 @@ class IntroActivity : AppCompatActivity() {
                 startActivity(intent)
             }else{
                 // 처음 접속하는 유저
+                binding.fragmentContainerView.visibility = View.VISIBLE
             }
         })
     }
