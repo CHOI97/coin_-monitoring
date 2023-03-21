e551e7d4b0fa44bf29291486a3d5b2e5e0d37211
CHOI97 <chlwjrh@gmail.com>
2023-03-22 02:35:11 +0900

    [feat] add  local database ROOM, Database Repository

---
+++ coin_monitoring/app/src/main/java/com/example/coin_monitoring/db/CoinPriceDatabase.kt
@@ -0,0 +1,34 @@
+package com.example.coin_monitoring.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.coin_monitoring.db.dao.InterestCoinDAO
+import com.example.coin_monitoring.db.entity.InterestCoinEntity
+
+@Database(entities = [InterestCoinEntity::class], version = 1)
+abstract class CoinPriceDatabase : RoomDatabase(){
+    abstract  fun interestCoinDao() : InterestCoinDAO
+
+    companion object{
+
+        @Volatile
+        private var INSTANCE : CoinPriceDatabase? =null
+        fun getDatabase(
+            context: Context
+        ): CoinPriceDatabase{
+            return INSTANCE ?: synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    CoinPriceDatabase::class.java,
+                    "coin_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
---
+++ coin_monitoring/app/src/main/java/com/example/coin_monitoring/db/dao/InterestCoinDAO.kt
@@ -0,0 +1,25 @@
+package com.example.coin_monitoring.db.dao
+
+import androidx.room.*
+import com.example.coin_monitoring.db.entity.InterestCoinEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface InterestCoinDAO {
+
+    // getAllData
+    // Flow ( Coroutine ) ROOM 과 같이 사용하면 데이터의 변경 사항을 감지하기 용이하다.
+    @Query("SELECT * FROM interest_coin_table")
+    fun getAllData() : Flow<List<InterestCoinEntity>>
+
+    // Insert
+    @Insert(onConflict =  OnConflictStrategy.IGNORE)
+    fun insert(interestCoinEntity: InterestCoinEntity)
+    // Update
+    @Update
+    fun update(interestCoinEntity: InterestCoinEntity)
+    // getSelectedCoinList -> 내가 관심있어하는 코인 데이터 가져오기
+    // coin1 , coin2 , coin3 -> coin1 data , coin2 data , coin data
+    @Query("SELECT * FROM insert_coin_table WHERE selected = :selected")
+    fun getSelectedData(selected: Boolean = true): List<InterestCoinEntity>
+}
