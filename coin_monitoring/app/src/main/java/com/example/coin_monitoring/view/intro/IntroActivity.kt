19d8f0a45eb38b6524b32ef45ef336d72fe37cbc
CHOI97 <chlwjrh@gmail.com>
2023-03-22 02:10:46 +0900

    [feat] Splash Lottie Animation 추가

--- coin_monitoring/app/src/main/java/com/example/coin_monitoring/view/intro/IntroActivity.kt
+++ coin_monitoring/app/src/main/java/com/example/coin_monitoring/view/intro/IntroActivity.kt
@@ -26,7 +26,6 @@ class IntroActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         binding = ActivityIntroBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
 
         Timber.d("onCreate")
@@ -39,6 +38,7 @@ class IntroActivity : AppCompatActivity() {
                 startActivity(intent)
             }else{
                 // 처음 접속하는 유저
+                binding.animationView.visibility = View.INVISIBLE
                 binding.fragmentContainerView.visibility = View.VISIBLE
             }
         })
