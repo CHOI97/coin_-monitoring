303fac320b555e92e81d5a18cb0cf0e9d7af6d7c
CHOI97 <chlwjrh@gmail.com>
2023-03-21 16:53:53 +0900

    [feat] DataStore, App instance

--- coin_monitoring/app/src/main/java/com/example/coin_monitoring/App.kt
+++ coin_monitoring/app/src/main/java/com/example/coin_monitoring/App.kt
@@ -1,10 +1,20 @@
 package com.example.coin_monitoring
 
 import android.app.Application
+import android.content.Context
 import timber.log.Timber
 
 class App: Application() {
 
+    init{
+        instance = this
+    }
+    companion object{
+        private var instance: App? = null
+        fun context() : Context {
+            return instance!!.applicationContext
+        }
+    }
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
---
+++ coin_monitoring/app/src/main/java/com/example/coin_monitoring/dataStore/MyDataStore.kt
@@ -0,0 +1,5 @@
+package com.example.coin_monitoring.dataStore
+
+class MyDataStore {
+    
+}
