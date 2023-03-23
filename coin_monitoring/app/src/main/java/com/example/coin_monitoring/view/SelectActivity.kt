c40594c2e688e36bbbef65122dc63f5483804d9a
CHOI97 <chlwjrh@gmail.com>
2023-03-23 20:01:13 +0900

    [feat] background unique task

--- coin_monitoring/app/src/main/java/com/example/coin_monitoring/view/SelectActivity.kt
+++ coin_monitoring/app/src/main/java/com/example/coin_monitoring/view/SelectActivity.kt
@@ -6,10 +6,13 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.work.*
+import com.example.coin_monitoring.background.GetCoinPriceRecentContractedWorkManager
 import com.example.coin_monitoring.view.main.MainActivity
 import com.example.coin_monitoring.databinding.ActivitySelectBinding
 import com.example.coin_monitoring.view.adapter.SelectRVAdapter
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 class SelectActivity : AppCompatActivity() {
 
@@ -37,9 +40,25 @@ class SelectActivity : AppCompatActivity() {
         }
         viewModel.save.observe(this, Observer{
             if(it.equals("done")){
+
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
+
+                // 가장 처음으로 우리가 저장한 코인 정보가 시작되는 시점
+                saveInterestCoinDataPeriodic()
             }
         })
+    }
+
+    private fun saveInterestCoinDataPeriodic(){
+        val myWork = PeriodicWorkRequest.Builder(
+            GetCoinPriceRecentContractedWorkManager::class.java,
+            15,
+            TimeUnit.MINUTES
+        ).build()
+        WorkManager.getInstance(this).enqueueUniquePeriodicWork(
+            "GetCoinPriceRecentContractedWorkManager",
+        ExistingPeriodicWorkPolicy.KEEP
+            ,myWork)
     }
 }
