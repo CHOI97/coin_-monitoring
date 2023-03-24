6d315e4963f5a35feb412664cfc883938694c9f4
CHOI97 <chlwjrh@gmail.com>
2023-03-24 12:49:07 +0900

    [feat] Service

---
+++ coin_monitoring/app/src/main/java/com/example/coin_monitoring/view/setting/SettingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.coin_monitoring.view.setting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.coin_monitoring.R
+
+class SettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+    }
+}
