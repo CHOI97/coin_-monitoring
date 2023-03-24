3a3659a1ff1bfe3687b7c25cdfa2be9892cc1cf6
CHOI97 <chlwjrh@gmail.com>
2023-03-24 13:18:20 +0900

    [feat] Add  Foreground Notification

--- coin_monitoring/app/src/main/java/com/example/coin_monitoring/service/PriceForegroundService.kt
+++ coin_monitoring/app/src/main/java/com/example/coin_monitoring/service/PriceForegroundService.kt
@@ -1,12 +1,22 @@
 package com.example.coin_monitoring.service
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import com.example.coin_monitoring.R
+import com.example.coin_monitoring.view.main.MainActivity
 import timber.log.Timber
 
 class PriceForegroundService: Service() {
 
+    private val NOTIFICATION_ID = 10000
     override fun onCreate() {
         super.onCreate()
     }
@@ -16,6 +26,7 @@ class PriceForegroundService: Service() {
 
             "START" ->{
                 Timber.d("START")
+                startForeground(NOTIFICATION_ID,makeNotification())
             }
             "STOP" ->{
                 Timber.d("START")
@@ -25,5 +36,39 @@ class PriceForegroundService: Service() {
     }
     override fun onBind(intent: Intent?): IBinder? {
         return null
+    }
+
+     fun makeNotification(): Notification{
+
+        val intent = Intent(this,MainActivity::class.java).apply{
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val pendingIntent : PendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val builder = NotificationCompat.Builder(this,"CHANNEL_ID")
+            .setSmallIcon(R.drawable.ic_baseline_access_alarms_24)
+            .setContentTitle("title")
+            .setContentText("content")
+            .setContentIntent(pendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val name = "name"
+            val descriptionText = "descriptionText"
+            val importance = NotificationManager.IMPORTANCE_LOW
+            val channel = NotificationChannel("CHANNEL_ID",name,importance).apply{
+                description = descriptionText
+            }
+            val notificationManager : NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+        return builder.build()
     }
 }
